d6900544145015e8b5deb6f4a0bf41ebc71e000e
Brandon Prange <bprange93@gmail.com>
2021-04-13 18:43:14 -0500

    Downloaded code runner to be able to run code faster. Comment for how to run it faster was put in for future reference.

--- app.py
+++ app.py
@@ -1,3 +1,2 @@
-x = 1
-y = 2
-unit_price = 3
+print("Hello World!")
+# use control alt n to be able to use the code runner to run your code faster instead of using the terminal since code runner downloaded.
