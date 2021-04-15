20de04a7238040a85c8d1f420fde00cc66eec0b9
Brandon Prange <bprange93@gmail.com>
2021-04-14 19:58:47 -0500

    Went over variables, how to set variables, setting 2 variables the same, and using multiple lines for a string.

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-print("Hello World!")
 # use control alt n to be able to use the code runner to run your code faster instead of using the terminal since code runner downloaded.
+students_count = 1000  # int
+rating = 4.99
+is_published = False
+course_name = "Python"
+multiple_line_string = """
+Multiple
+Lines
+"""
+
+x = 1
+y = 2
+# line below same as the 2 lines above
+x, y = 1, 2
+# both variables set to the same number. Can set 2 variables this way if they will both be the same thing
+x = y = 1
