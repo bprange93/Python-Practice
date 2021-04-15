5c6c817195453a561b38dbdccae06dca543ad08b
Brandon Prange <bprange93@gmail.com>
2021-04-15 15:44:34 -0500

    Learned the different ways to use strings using the index and how to show it.

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 # use control alt n to be able to use the code runner to run your code faster instead of using the terminal since code runner downloaded.
-students_count = 1000  # int
-# shows the type of variable it is and prints in terminal.
-print(type(students_count))
-rating = 4.99
-is_published = False
-course_name = "Python"
-multiple_line_string = """
-Multiple
-Lines
-"""
+
 # python is a dynamic language which means variables are declared at run time not compile time.
 # C# is a static language since the variables need to be declared before running.
 
-# intergers are immutable
-x = [1, 2, 3]
-print(id(x))
-# lists are mutable but for some reason is stored in different place on here instead of in same like in video.
-x.append(4)
-print(id(x))
+course = "Python Programming"
+print(len(course))
+print(course[0])
+# Shows the 2nd to last letter in the index since going negative
+print(course[-2])
+# Will show the first 3 from the 0 index to the 2 index (pyt)
+print(course[:3])
+# will show the whole length of the string
+print(course[0:])
+# Is the same as the line above
+print(course[:])
