c7aa9b647357337c836c661ee4d56a2df1d4e5e2
Brandon Prange <bprange93@gmail.com>
2021-04-21 12:54:56 -0500

    Went over escape key. It is the backslash \. you can use it to insert a new line, use it to insert a special key such as a double quote inside of double quotes, or use \n to create a new line.

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 # Short cut to run code Ctrl Alt n.
 # Using the single quotes will allow you to use the double quote inside and print it.
-message = 'Python "Programming'
+# Backslash is a special character. It is known as an escape sequence
+# \n means new line. If used in message it will show programming on the next line.
+# This way seemed to have created a little indent because of the space when run.
+message = "Python \n Programing"
+# another cleaner way to do the same thing.
+message2 = """
+Python 
+Programing
+"""
 print(message)
+print(message2)
