817550e99e06c1fd284d4a329ca924fc08981327
Brandon Prange <bprange93@gmail.com>
2021-04-21 13:14:20 -0500

    Learned about formatted strings. These can be used many different ways. Some I put in the code for later examples if needed along with some comments describing how some of it all works.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # Short cut to run code Ctrl Alt n.
-# Using the single quotes will allow you to use the double quote inside and print it.
-# Backslash is a special character. It is known as an escape sequence
-# \n means new line. If used in message it will show programming on the next line.
-# This way seemed to have created a little indent because of the space when run.
-message = "Python \n Programing"
-# another cleaner way to do the same thing.
-message2 = """
-Python 
-Programing
-"""
-print(message)
-print(message2)
+first = "Brandon"
+last = "Prange"
+# This is the same as setting variable to print = to first + " " + last.
+# Cleaner way to doing it and not having to worry about the space with quotes.
+# This is a formatted string. It can be used many different ways and can call methods inside of it.
+
+full = f"{first} {last}"
+full2 = f"{len(first)} {last}"
+full3 = f"{len(first)} {2 + 2}"
+print(full)
+print(full2)
+print(full3)
