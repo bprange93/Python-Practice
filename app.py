8a84bdc2630d76623214d06e6331352958c7dad9
Brandon Prange <bprange93@gmail.com>
2021-04-21 12:21:30 -0500

    Put the short cut to run code in a comment, as well as how to put double quotes inside of a string and get it to show. You put them inside of single quotes as shown in the code.

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-# use control alt n to be able to use the code runner to run your code faster instead of using the terminal since code runner downloaded.
-
-# python is a dynamic language which means variables are declared at run time not compile time.
-# C# is a static language since the variables need to be declared before running.
-
-course = "Python Programming"
-print(len(course))
-print(course[0])
-# Shows the 2nd to last letter in the index since going negative
-print(course[-2])
-# Will show the first 3 from the 0 index to the 2 index (pyt)
-print(course[:3])
-# will show the whole length of the string
-print(course[0:])
-# Is the same as the line above
-print(course[:])
+# Short cut to run code Ctrl Alt n.
+# Using the single quotes will allow you to use the double quote inside and print it.
+message = 'Python "Programming'
+print(message)
