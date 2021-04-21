24f450366095068f15499a2ff29e6e10098ac983
Brandon Prange <bprange93@gmail.com>
2021-04-21 15:20:58 -0500

    Went over numbers. Import math so I could use some of the math methods, as well as learned some methods that can be used with integers or float numbers.

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
 # Short cut to run code Ctrl Alt n.
-# addition
-x = 10 + 3
-# subtraction
-x = 10 - 3
-# multiplication
-x = 10 * 3
-# division that gives a decimal if anything is left over
-x = 10 / 3
-# division that gives a whole number even if there is anything left over.
-# Rounds to the nearest whole number.
-x = 10 // 3
-# Modulous
-x = 10 % 3
-# multiplaction to the power of (Exponent)
-# will = 1000 since its 10 x 10 x 10.
-x = 10 ** 3
-# Both under statements are the same.
-x = x + 1
-x += 1
-# in python there are no increment or decrement operators (++ or --).
-print(x)
+# Using upper case letters lets other developers know that this variable is a constant.
+# if want to see all of the built in funcitons go to google and make sure to use the number of python to look them all up.
+import math
+PI = -3.14
+print(round(PI))
+print(abs(PI))
+
+print(math.floor(PI))
