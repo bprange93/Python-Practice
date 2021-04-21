a732c102d310d4ea9c6feea0b2d7c2d245fbd8bd
Brandon Prange <bprange93@gmail.com>
2021-04-21 14:34:31 -0500

    Learned about the different types of operators in python. How they differ from how they are used or can be used in C#. Different ways to write the same thing that might be cleaner or make more sense to certain people.

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 # Short cut to run code Ctrl Alt n.
-x = 10
-# binary number. 0b10 is the binary number for 2.
-x = 0b10
-# this prints the number as a binary number when you preface it with bin.
-print(bin(x))
-# hexadecimal number. this one equals 300
-x = 0x12c
-# to print it as a hexidecimal number when using it use hex()
-print(hex(x))
-
-# complex numbers (a + bi) Really won't ever be used.
-
-x = 1 + 2j
+# addition
+x = 10 + 3
+# subtraction
+x = 10 - 3
+# multiplication
+x = 10 * 3
+# division that gives a decimal if anything is left over
+x = 10 / 3
+# division that gives a whole number even if there is anything left over.
+# Rounds to the nearest whole number.
+x = 10 // 3
+# Modulous
+x = 10 % 3
+# multiplaction to the power of (Exponent)
+# will = 1000 since its 10 x 10 x 10.
+x = 10 ** 3
+# Both under statements are the same.
+x = x + 1
+x += 1
+# in python there are no increment or decrement operators (++ or --).
 print(x)
