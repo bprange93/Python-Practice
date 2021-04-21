a1aa960f6dcb370d38f7c9ef21ace941c0a91db1
Brandon Prange <bprange93@gmail.com>
2021-04-21 13:35:54 -0500

    Played around with some of the string methods. Title capatilizes the first letter in every word, and you can find the position and replace. Also can print a bool if a word is in the string or not.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # Short cut to run code Ctrl Alt n.
-first = "Brandon"
-last = "Prange"
-# This is the same as setting variable to print = to first + " " + last.
-# Cleaner way to doing it and not having to worry about the space with quotes.
-# This is a formatted string. It can be used many different ways and can call methods inside of it.
+course = "      Python Programming"
+print(course.upper())
+print(course.lower())
+# title used to upper case first letter in every word.
+print(course.title())
 
-full = f"{first} {last}"
-full2 = f"{len(first)} {last}"
-full3 = f"{len(first)} {2 + 2}"
-print(full)
-print(full2)
-print(full3)
+print(course.strip())
+
+print(course.find("Pro"))
+print(course.replace("P", "-"))
+
+print("Programming" in course)
+print("Programming" not in course)
