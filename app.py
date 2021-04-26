20eb08d7349bf627bb6ac42757d266d63260ed13
Brandon Prange <bprange93@gmail.com>
2021-04-26 15:31:47 -0500

    Went over how to use a for loop. Kind of works the same as an if statement with a slight variance. Seems to be much easier than in c# though.

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-# Chain Comparison operators
-
-# age should be between 18 and 65
-
-age = 22
-if age >= 18 and age < 65:
-    print("Eligible")
-# This line does the same as teh line above.
-# This line is cleaner and this is how we chain comparison operators.
-if 18 <= age < 65:
+for number in range(1, 10, 2):
+    print("Attempt", number, number * ".")
