13a074a73db3854296083904f9a79c30481550ef
Brandon Prange <bprange93@gmail.com>
2021-04-26 14:28:50 -0500

    Learning some short cuts on how to make the code cleaner. Like this way better since it is making the message the if statement which takes away multiple lines of code and sets it as one line.

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 # Short cut to run code Ctrl Alt n.
 # Using upper case letters lets other developers know that this variable is a constant.
-temperature = 15
-# when using an if statment always terminate the expression with a ( : )
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 20:
-    print("It's nice")
-else:
-    print("It's cold")
-print("Done")
+age = 12
+message = "Eligible" if age >= 18 else "Ineligible"
+print(message)
