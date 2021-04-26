a1244fa7abba71a8cb52c215f1deffa3d6d1cf7e
Brandon Prange <bprange93@gmail.com>
2021-04-26 14:00:19 -0500

    Went over type conversions and talked about which instances of a boolean will turn up immediately false while everything else will be true. Even if you set the str (False) this will come back as true.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Short cut to run code Ctrl Alt n.
 # Using upper case letters lets other developers know that this variable is a constant.
 # if want to see all of the built in funcitons go to google and make sure to use the number of python to look them all up.
-import math
-PI = -3.14
-print(round(PI))
-print(abs(PI))
-
-print(math.floor(PI))
+x = input("x: ")
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+# If used in boolean context it will turned up false, anything else will be true.
+# falsy
+# "", 0, None
