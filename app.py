dc7b852e5c67a41f1ff2449ee1205bec1b14969a
Brandon Prange <bprange93@gmail.com>
2021-04-26 14:23:29 -0500

    Worked with if statements. Used elif (else if) and also else. I didn't put a comment in there but if it is in the if, elif, or else statement it needs to be indented in order for it to be used in the statement. If the indentation is taken away it isn't included inside of the statement anymore. Notice how the print(Done) isn't. That will be run every time since it is after the statements.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # Short cut to run code Ctrl Alt n.
 # Using upper case letters lets other developers know that this variable is a constant.
-# if want to see all of the built in funcitons go to google and make sure to use the number of python to look them all up.
-x = input("x: ")
-y = int(x) + 1
-print(f"x: {x}, y: {y}")
-# If used in boolean context it will turned up false, anything else will be true.
-# falsy
-# "", 0, None
+temperature = 15
+# when using an if statment always terminate the expression with a ( : )
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
