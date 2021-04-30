236cf1fcaf98e3819ad276b1d844dfe9858cb0c2
Brandon Prange <bprange93@gmail.com>
2021-04-30 13:09:41 -0500

    Went to work on Kata. Couldn't figure it out. comment out what had down. Watched tutorial based on java trying to convert. Look up for loops for python. Go over if statements from earlier commits when come back to. Kata name Does my number look big in this?

--- Python-Sandbox.py
+++ Python-Sandbox.py
@@ -1,2 +1,21 @@
+import math
+
 str = "Batman"
 print(str[-1::-1])
+
+
+# def narcissistic(value):
+# split up then multiplied by exponent of digits of number
+#x = 0
+#nw = 0
+# if value > 9:
+#    x = value.str.split
+#    for i = 0, i > x.length, i + 1:
+#        nw += math.pow(x=-1 y=-1):
+# if nw == = value:
+#    return True
+# else:
+#    return False
+
+
+# print(narcissistic(371))
