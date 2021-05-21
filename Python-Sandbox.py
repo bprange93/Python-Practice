fdd6d376f8eaf72db441c18f3a6ed521a7d134e1
Brandon Prange <bprange93@gmail.com>
2021-05-21 12:30:53 -0500

    Started working on another kata in code wars to present thought process and how to solve problem. Worked some of the time but not every time. Debugging to find out why and how to fix it. Kata called Last Survivor.

--- Python-Sandbox.py
+++ Python-Sandbox.py
@@ -18,6 +18,11 @@ import string
 # else:
 #    return False
 
+# first I want to make an array to hold all of the letters in a position
+# then I want to make a method that takes the poisionts in the array of coors and remove that position
+# For loop through all of them till 1 is remaining
+# return last letter
+
 
 # print(narcissistic(371))
 
@@ -153,5 +158,27 @@ def is_pangram(s):
         else:
             return False
 
+# Detect Pangram codewars
+#print(is_pangram("The quick, brown fox jumps over the lazy dog"))
 
-print(is_pangram("The quick, brown fox jumps over the lazy dog"))
+
+def last_survivor(letters, coords):
+    # take in the string and turn it into an array
+    # each letter will be own character and have coorodinate that will need to be removed
+    # for loop through removing letters till only one is left
+    # return the final letter
+    letterArray = []
+    overIndex = []
+    for letter in letters:
+        letterArray.append(letter)
+    for coord in coords:
+        letterArray.remove(letterArray[coord])
+        if coord == 0:
+            letterArray.remove(letterArray[0])
+            break
+
+    return letterArray
+
+
+print(last_survivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr', [8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25,
+      36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1]))
