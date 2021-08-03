8b3cb992da6acd0fe561c4301a78ad482b7edc5f
Brandon Prange <bprange93@gmail.com>
2021-08-03 15:01:39 -0500

    Figured out the way they actually wanted it. Next will be working on the plus minus times and divideby functions.

--- Python-Sandbox.py
+++ Python-Sandbox.py
@@ -263,36 +263,48 @@ def solution(n):
     return output
 
 
-def numbers():
-    numberValue = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    numberWord = ['one', 'two', 'three', 'four',
-                  'five', 'six', 'seven', 'eight', 'nine']
-    for number in numberWord:
-        numberValue[number] = numberWord[number]
+# def numbers():
+#     numberValue = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#     numberWord = ['one', 'two', 'three', 'four',
+#                   'five', 'six', 'seven', 'eight', 'nine']
+#     for number in numberWord:
+#         numberValue[number] = numberWord[number]
 
 
 def one():
+    return 1
 
 
 def two():
+    return 2
 
 
 def three():
+    return 3
 
 
 def four():
+    return 4
 
 
 def five():
+    return 5
 
 
 def six():
+    return 6
 
 
 def seven():
+    return 7
 
 
 def eight():
+    return 8
 
 
 def nine():
+    return 9
+
+
+print(one())
